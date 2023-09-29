cc992f94f3a0b1bae7a2d3a19a9b7c1977e1e836
Anish22prakash <anish2272000@gmail.com>
2023-09-29 16:05:03 +0530

    added demo comment

--- BackEnd/DharmikBackEndOyo/DharmikBackEndOyo/Models/Facilites.cs
+++ BackEnd/DharmikBackEndOyo/DharmikBackEndOyo/Models/Facilites.cs
@@ -1,6 +1,24 @@
-﻿namespace DharmikBackEndOyo.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DharmikBackEndOyo.Models
 {
     public class Facilites
     {
+        [Key]
+        public int FacilitesId { get; set; }
+
+        [Required(ErrorMessage = "Please enter Facilites Name")]
+        public string FacilitesName { get; set; }
+
+        [Required]
+        public int DharmShalaId { get; set; }
+
+        [ForeignKey("DharmShalaId")]
+        public Dharmshala Dharmshala { get; set; }
+
+        public DateTime InsertDateTime { get; set; }
+
+        public DateTime UpdateDateTime { get; set; }
     }
 }
